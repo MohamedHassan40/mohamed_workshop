eca658045e1be92f61b4a20b6d7d76cef04f801f
MohamedHassan40 <mohamedelorbany10@gmail.com>
2025-06-03 02:39:40 +0300

    tst

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import json
 
-app = Flask(__name__)
 app.secret_key = "aodsiasdioaosd"
 
 # Make `data` available in all templates
